3c7bc5d36f2808665f42fdf4e428df980aa7c690
Trung Minh <teen0006@gmail.com>
2017-11-30 17:42:39 +0700

    fix port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('./config/dependencies');
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log('listenning in port 5000...');
+    console.log('listenning in port: ' + process.env.PORT);
 });
