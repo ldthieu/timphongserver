2ef0d974aa167b7ebf2ffe0014ab70dcf0fe6560
Trung Minh <teen0006@gmail.com>
2017-12-05 15:49:52 +0700

    cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //var app = require('./config/dependencies');
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var route = require('./routes/index');
 var { port_api } = require('./config/config')
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
